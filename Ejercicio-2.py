1a07712913f5e7f94046a23e246938475fc4a119
Addi Trejo <125314660+Additrejo@users.noreply.github.com>
2023-10-13 20:26:43 -0600

    Update Ejercicio-2.py
    Agregamos plantilla de descripción.

--- Ejercicio-2.py
+++ Ejercicio-2.py
@@ -1,5 +1,16 @@
+'''
+Samsung innovation campus.
+Autor: Equipo 4. Juan, Addi, Aldo.
+Programa: Ejercicio 2.
+Creado: 13/Octubre/2023. 
+Versión: 1.0
+Descrpción: Programa que con el tiempo escrito en la consola, refleje el led encendido ese tiempo Requistos: LED GPIO17
+'''
+
 from gpiozero import LED
 import time
+
+#EJERCICIO  2. 
 
 # Define el pin GPIO al que está conectado el LED (en este caso, GPIO 17)
 led = LED(17)
@@ -8,8 +19,8 @@ try:
     # Pide al usuario que ingrese el tiempo en segundos
     tiempo_encendido = int(input("Ingresa el tiempo de encendido del LED (segundos): "))
     led.on()
-    print(f"LED encendido durante {tiempo_encendido} segundos...")
-    time.sleep(tiempo_encendido)
+    print(f"LED encendido durante {tiempo_encendido} segundos...") 
+    time.sleep(tiempo_encendido)    #10000 ms
     led.off()
     print("LED apagado")
 
